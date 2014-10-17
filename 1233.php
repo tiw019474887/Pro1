fa018d1cec6871f6d8a720fc8728d1f237bc5f77
tiw019474887 <tiw_t1@hotmail.com>
2014-10-18 01:36:43 +0700

    tiwter

--- 1233.php
+++ 1233.php
@@ -102,6 +102,19 @@ body,td,th {
         </form></td>
       <td width="675">
 		<?php
+		$allowed_types=array('jpg','jpeg','gif','png');  
+$dir    ="ก๋วยเตี๋ยว.jpg";  
+$files1 = scandir($dir);  
+foreach($files1 as $key=>$value){  
+    if($key>1){  
+        $file_parts = explode('.',$value);  
+        $ext = strtolower(array_pop($file_parts));  
+        if(in_array($ext,$allowed_types)){  
+            echo "<img style='width:100px;' src='".$dir.$value."'/>&nbsp;";     
+        }  
+  
+    }  
+}  
 		if(isset($_POST['select'])){
 		$selected_val = $_POST['select'];  
 		echo "เส้น :     " .$selected_val."<br>";
